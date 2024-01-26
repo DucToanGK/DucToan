492134313891808286a27b9044146e6fb1032d71
Đức Toàn <ductoangk2004@gmail.com>
2024-01-26 17:41:19 +0700

    fix

--- Test/Test/Class1.cs
+++ Test/Test/Class1.cs
@@ -8,5 +8,9 @@ namespace Test
 {
     internal class Class1
     {
+        int x;
+        int y;
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
     }
 }
